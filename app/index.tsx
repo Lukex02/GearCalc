c14ce39749000f301dde7640401d95473951f124
Lukex02 <minh.huynhba2004@hcmut.edu.vn>
2025-03-13 20:42:13 +0700

    remove test preview

--- app/index.tsx
+++ app/index.tsx
@@ -1,33 +1,59 @@
-import React, { useState } from "react";
-import { Text, View, Button } from "react-native";
-import Engine, { Efficiency } from "./src/EngiCalc";
+// import React, { useState } from "react";
+// import { Text, View, Button } from "react-native";
+// import Engine, { Efficiency } from "./src/EngiCalc";
+import { Text, View } from "react-native";
 
+// export default function Index() {
+//   const [engine, setEngine] = useState<Engine | null>(null);
+//   const [efficiency, setEfficiency] = useState<Efficiency | null>(null);
+
+//   const calculateEfficiency = () => {
+//     const newEfficiency = new Efficiency(0.99, 0.85, 0.96, 0.94, 0.98);
+//     setEfficiency(newEfficiency);
+//     return newEfficiency;
+//   };
+
+//   const calculatePower = () => {
+//     const newEngine = new Engine(
+//       17000,
+//       0.5,
+//       15,
+//       120,
+//       10,
+//       1,
+//       25,
+//       0.5,
+//       15,
+//       calculateEfficiency().get_effi_system()
+//     );
+//     setEngine(newEngine);
+//   };
+
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+//       }}
+//     >
+//       <Button title="Tính Công Suất" onPress={calculatePower} />
+//       {efficiency && (
+//         <Text>
+//           Hiệu suất truyền động hệ thống:{" "}
+//           {efficiency.get_effi_system().toFixed(3)}
+//         </Text>
+//       )}
+//       {engine && (
+//         <Text>
+//           Công suất cần thiết: {engine.get_p_ct().toFixed(2)} {"\n"}
+//           Số vòng quay sơ bộ của động cơ: {engine.get_n_sb().toFixed(0)}
+//         </Text>
+//       )}
+//     </View>
+//   );
+// }
 export default function Index() {
-  const [engine, setEngine] = useState<Engine | null>(null);
-  const [efficiency, setEfficiency] = useState<Efficiency | null>(null);
-
-  const calculateEfficiency = () => {
-    const newEfficiency = new Efficiency(0.99, 0.85, 0.96, 0.94, 0.98);
-    setEfficiency(newEfficiency);
-    return newEfficiency;
-  };
-
-  const calculatePower = () => {
-    const newEngine = new Engine(
-      17000,
-      0.5,
-      15,
-      120,
-      10,
-      1,
-      25,
-      0.5,
-      15,
-      calculateEfficiency().get_effi_system()
-    );
-    setEngine(newEngine);
-  };
-
   return (
     <View
       style={{
@@ -36,19 +62,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Button title="Tính Công Suất" onPress={calculatePower} />
-      {efficiency && (
-        <Text>
-          Hiệu suất truyền động hệ thống:{" "}
-          {efficiency.get_effi_system().toFixed(3)}
-        </Text>
-      )}
-      {engine && (
-        <Text>
-          Công suất cần thiết: {engine.get_p_ct().toFixed(2)} {"\n"}
-          Số vòng quay sơ bộ của động cơ: {engine.get_n_sb().toFixed(0)}
-        </Text>
-      )}
+      <Text>Lorem ipsum</Text>
     </View>
   );
 }
