13d60592312d627572994a4f4fae8481737b4549
Lukex02 <minh.huynhba2004@hcmut.edu.vn>
2025-03-17 14:52:05 +0700

    Add template gearbox builder

--- app/src/models/GearBox.tsx
+++ app/src/models/GearBox.tsx
@@ -1,1 +1,38 @@
+import { SelectedEngine } from "./EngineModel";
+
+class GearBox {
+  private _engine!: SelectedEngine;
+
+  set engine(engine: SelectedEngine) {
+    this._engine = engine;
+  }
+}
+
 // This is where the builder of Gear Box will be created
+interface Builder {
+  reset(): void;
+  setEngine(engine: SelectedEngine): void;
+  // setGears(gears: Gear[]): void;
+  // setShaft(shaft: Shaft): void;
+  build(): GearBox;
+}
+
+class GearBoxBuilder implements Builder {
+  private _gearBox!: GearBox;
+
+  constructor() {
+    this.reset();
+  }
+
+  reset(): void {
+    this._gearBox = new GearBox();
+  }
+
+  setEngine(engine: SelectedEngine): void {
+    this._gearBox.engine = engine;
+  }
+
+  build(): GearBox {
+    return this._gearBox;
+  }
+}
