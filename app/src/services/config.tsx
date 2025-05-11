43cc5cdecbea6e96c26be0821f954c1bf5247ef7
Lukex02 <minh.huynhba2004@hcmut.edu.vn>
2025-05-11 14:25:01 +0700

    welp

--- app/src/services/config.tsx
+++ app/src/services/config.tsx
@@ -2,9 +2,7 @@ import Constants from "expo-constants";
 import { createClient, User } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://odrfusjgbctkybkavelg.supabase.co";
-// const supabaseKey = Constants.expoConfig?.extra?.supabaseAnonKey;
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kcmZ1c2pnYmN0a3lia2F2ZWxnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0Njk0NTM0MiwiZXhwIjoyMDYyNTIxMzQyfQ.QFghw7GsfiIH68YKD5laFt7UabVxBwbhMihbHZPx5V0";
+const supabaseKey = Constants.expoConfig?.extra?.supabaseAnonKey;
 const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
     autoRefreshToken: true,
